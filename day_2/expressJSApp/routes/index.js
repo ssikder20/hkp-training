3d76c0333ac09803e832f0a3670c7ca9be47c4fa
Shuprovo Sikder <ssikder20@stuy.edu>
2020-09-26 00:42:52 -0400

    created language page; post form

--- day_2/expressJSApp/routes/index.js
+++ day_2/expressJSApp/routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 // Grab handlers for routes
-let index = require('../controllers/index.js');
+let landing = require('../controllers/landing');
 
 /* GET home page. */
-router.get('/', index.index);
+router.get('/', landing.get_landing);
 
 module.exports = router;
