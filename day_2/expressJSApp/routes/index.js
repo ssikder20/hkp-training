d437894c56806240ae508fac067f2c09257473b7
Shuprovo Sikder <golpos369@gmail.com>
2020-09-25 20:05:42 -0400

    added nodemon for hot-refreshing

--- day_2/expressJSApp/routes/index.js
+++ day_2/expressJSApp/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+// Grab handlers for routes
+let index = require('../controllers/index.js');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index.index);
 
 module.exports = router;
