4b0d1c1316fe39cb52254a444d809425e4e32fda
Shuprovo Sikder <ssikder20@stuy.edu>
2020-10-01 18:41:08 -0400

    added remove route

--- day_3/restAPIJWT/routes/cart.js
+++ day_3/restAPIJWT/routes/cart.js
@@ -16,4 +16,19 @@ router.get("/items/get/:token", verify.verifyParam, async (req, res, next) => {
   }
 });
 
+// Remove an item from user's cart using item id
+router.post("/items/remove", verify.verifyBody, async (req, res, next) => {
+  const itemId = req.body._id; // Get item id from request body
+
+  await Item.deleteOne({ _id: itemId }); // Remove item from database
+
+  // Find items in database matching username and sent as JSON object
+  try {
+    const cart = await Item.find({ username: req.user.username }); // Get a list of items assigned to a username
+    res.send({ items: cart });
+  } catch (err) {
+    res.status(400).send({ message: err });
+  }
+});
+
 module.exports = router;
