e55107e21253596a55efb00e92f680afe1d346ce
Shuprovo Sikder <ssikder20@stuy.edu>
2020-10-06 01:54:26 -0400

    added update notes route

--- day_4/notes-app-api/list.js
+++ day_4/notes-app-api/list.js
@@ -1,4 +1,4 @@
-import handler from "./libs/handler-libs";
+import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
